a60befdc1cfafbca32a3ba6bd5e49e80477c2280
Byte-Sized Euclid <8bit.euclid@gmail.com>
2022-08-31 17:04:50 +0200

    Compiling Apeiron with OpenVDB and ran simple test case.

--- src/main.cpp
+++ src/main.cpp
@@ -25,77 +25,128 @@
 using namespace aprn;
 using namespace aprn::vis;
 
-int main(void)
+#include <openvdb/openvdb.h>
+
+using namespace openvdb;
+
+void makeCylinder(FloatGrid::Ptr grid, float radius, const CoordBBox& indexBB, double h)
 {
-//   flmgr::CopyFile("./libs/Visualiser/resources/latex/render_text_template.tex", "./libs/Visualiser/resources");
-//   flmgr::File file;
-//   file.Open("./libs/Visualiser/resources/render_text_template.tex", flmgr::Mode::Append);
-//
-//   file.Write("\nTEST\n\\end{document}");
-//   file.Close();
-//
-//   flmgr::CompileTeXFile("lualatex", "./libs/Visualiser/data/render_text.tex");
-//   flmgr::ConvertPDFtoPNG("./libs/Visualiser/data/render_text.pdf", 1000);
-//
-//   EXIT("TEST")
+   typename FloatGrid::Accessor accessor = grid->getAccessor();
 
-   Visualiser visualiser;
-   Scene scene(1000.0);
-   Model model;
+   for (Int32 i = indexBB.min().x(); i <= indexBB.max().x(); ++i) {
+      for (Int32 j = indexBB.min().y(); j <= indexBB.max().y(); ++j) {
+         for (Int32 k = indexBB.min().z(); k <= indexBB.max().z(); ++k) {
+            // transform point (i, j, k) of index space into world space
+            Vec3d p(i * h, j * h, k * h);
+            // compute level set function value
+            float distance = sqrt(p.x() * p.x() + p.y() * p.y()) - radius;
 
-//   scene.Add(TeXBox(R"(This is a test $e = mc^2$.)", ""));
-//   scene.Add(TeXBox(R"(This is another test: $p_4(x) = a_0 + a_1x + a_2x^2 + a_3x^3 + a_4x^4$.)", ""));
-//   scene.Add(TeXBox(R"(This is yet \textbf{another} test with a \textit{slightly} more complicated mathematical equation: $s = u\tau + \frac{1}{2}a\tau^2$.)", ""));
+            accessor.setValue(Coord(i, j, k), distance);
+         }
+      }
+   }
 
-//   scene.Add(TeXBox(R"($\sum$)", ""));
-   scene.Add(TeXBox(R"(Test $\tau$ and $\sum$,)", ""));
-//   scene.Add(TeXBox(R"(This is \textbf{Bold}, \textit{italic} polynomial: $p_2(x) = a_0 + a_1x + a_2x^2$. Equation with commands and a fraction: $s = u\tau + \frac{1}{2}a\tau^2$.)", ""));
+   grid->setTransform(openvdb::math::Transform::createLinearTransform(h));
+}
 
-   // Cube
-   model = ModelFactory::Cube(1.0);
-   model.SetColour({1.0, 1.0, 1.0})
-        .RotateAt({0.0f, 0.0f, 1.0f}, 2.0)
-        .Trace([](Float t){ return SVectorF3{ Three*std::sin(TwoThird * t), Zero, Zero }; }, 2.0);
-   scene.Add(model, "Cube");
+void createAndSaveCylinder()
+{
+   openvdb::initialize();
 
-   // Floor
-   model = ModelFactory::Square(10.0);
-   model.SetMaterial("Brick", 0.8, 256.0)
-        .SetTexture("Brick", "Wall", 1, 2)
-        .OffsetPosition({0.0f, -2.0f, 0.0f})
-        .OffsetOrientation(-HalfPi, {1.0f, 0.0f, 0.0f});
-   scene.Add(model);
+   openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create(2.0);
 
-   // Wall 0
-   model = ModelFactory::Square(5.0);
-   model.SetMaterial("Brick", 0.8, 256.0)
-        .SetTexture("Brick", "Wall", 1, 2)
-        .OffsetPosition({2.5f, 0.5f, -5.0f});
-   scene.Add(model);
+   CoordBBox indexBB(Coord(-20, -20, -20), Coord(20, 20, 20));
+   makeCylinder(grid, 5.0f, indexBB, 0.5);
 
-   // Wall 1
-   model = ModelFactory::Square(5.0);
-   model.SetMaterial("Brick", 0.8, 256.0)
-        .SetTexture("Brick", "Wall", 1, 2)
-        .OffsetPosition({-2.5f, 0.5f, -5.0f});
-   scene.Add(model)
-        .Add(PointLight(glm::vec3(0.0, -0.5, -2.0), glm::vec4(1.0, 1.0, 1.0, 1.0), 20.0, 2.5, {0.01, 0.5, 0.5}), "Sun");
-//        .Add(DirectionalLight(glm::vec3(0.0, -1.0, -1.0), glm::vec4(1.0, 1.0, 1.0, 1.0), 0.3, 0.6), "Sun");
+   // specify dataset name
+   grid->setName("LevelSetCylinder");
 
-//   // Floor
-//   model = ModelFactory::Square(10.0);
-//   model.SetMaterial("Paper", 0.1, 16.0)
-//        .SetColour({1, 1, 1});
-////        .SetTexture("Paper", "", 2, 4)
-////        .OffsetPosition({0.0f, -2.0f, 0.0f})
-////        .OffsetOrientation(-HalfPi, {1.0f, 0.0f, 0.0f});
-//   scene.Add(model);
+   // save grid in the file
+   openvdb::io::File file("mygrids.vdb");
+   openvdb::GridPtrVec grids;
+   grids.push_back(grid);
+   file.write(grids);
+   file.close();
+}
 
-   visualiser.Add(scene);
-   visualiser.Render();
+int main()
+{
+   createAndSaveCylinder();
 
    return 0;
 }
+
+//int main(void)
+//{
+////   flmgr::CopyFile("./libs/Visualiser/resources/latex/render_text_template.tex", "./libs/Visualiser/resources");
+////   flmgr::File file;
+////   file.Open("./libs/Visualiser/resources/render_text_template.tex", flmgr::Mode::Append);
+////
+////   file.Write("\nTEST\n\\end{document}");
+////   file.Close();
+////
+////   flmgr::CompileTeXFile("lualatex", "./libs/Visualiser/data/render_text.tex");
+////   flmgr::ConvertPDFtoPNG("./libs/Visualiser/data/render_text.pdf", 1000);
+////
+////   EXIT("TEST")
+//
+//   Visualiser visualiser;
+//   Scene scene(1000.0);
+//   Model model;
+//
+////   scene.Add(TeXBox(R"(This is a test $e = mc^2$.)", ""));
+////   scene.Add(TeXBox(R"(This is another test: $p_4(x) = a_0 + a_1x + a_2x^2 + a_3x^3 + a_4x^4$.)", ""));
+////   scene.Add(TeXBox(R"(This is yet \textbf{another} test with a \textit{slightly} more complicated mathematical equation: $s = u\tau + \frac{1}{2}a\tau^2$.)", ""));
+//
+////   scene.Add(TeXBox(R"($\sum$)", ""));
+//   scene.Add(TeXBox(R"(Test $\tau$ and $\sum$,)", ""));
+////   scene.Add(TeXBox(R"(This is \textbf{Bold}, \textit{italic} polynomial: $p_2(x) = a_0 + a_1x + a_2x^2$. Equation with commands and a fraction: $s = u\tau + \frac{1}{2}a\tau^2$.)", ""));
+//
+//   // Cube
+//   model = ModelFactory::Cube(1.0);
+//   model.SetColour({1.0, 1.0, 1.0})
+//        .RotateAt({0.0f, 0.0f, 1.0f}, 2.0)
+//        .Trace([](Float t){ return SVectorF3{ Three*std::sin(TwoThird * t), Zero, Zero }; }, 2.0);
+//   scene.Add(model, "Cube");
+//
+//   // Floor
+//   model = ModelFactory::Square(10.0);
+//   model.SetMaterial("Brick", 0.8, 256.0)
+//        .SetTexture("Brick", "Wall", 1, 2)
+//        .OffsetPosition({0.0f, -2.0f, 0.0f})
+//        .OffsetOrientation(-HalfPi, {1.0f, 0.0f, 0.0f});
+//   scene.Add(model);
+//
+//   // Wall 0
+//   model = ModelFactory::Square(5.0);
+//   model.SetMaterial("Brick", 0.8, 256.0)
+//        .SetTexture("Brick", "Wall", 1, 2)
+//        .OffsetPosition({2.5f, 0.5f, -5.0f});
+//   scene.Add(model);
+//
+//   // Wall 1
+//   model = ModelFactory::Square(5.0);
+//   model.SetMaterial("Brick", 0.8, 256.0)
+//        .SetTexture("Brick", "Wall", 1, 2)
+//        .OffsetPosition({-2.5f, 0.5f, -5.0f});
+//   scene.Add(model)
+//        .Add(PointLight(glm::vec3(0.0, -0.5, -2.0), glm::vec4(1.0, 1.0, 1.0, 1.0), 20.0, 2.5, {0.01, 0.5, 0.5}), "Sun");
+////        .Add(DirectionalLight(glm::vec3(0.0, -1.0, -1.0), glm::vec4(1.0, 1.0, 1.0, 1.0), 0.3, 0.6), "Sun");
+//
+////   // Floor
+////   model = ModelFactory::Square(10.0);
+////   model.SetMaterial("Paper", 0.1, 16.0)
+////        .SetColour({1, 1, 1});
+//////        .SetTexture("Paper", "", 2, 4)
+//////        .OffsetPosition({0.0f, -2.0f, 0.0f})
+//////        .OffsetOrientation(-HalfPi, {1.0f, 0.0f, 0.0f});
+////   scene.Add(model);
+//
+//   visualiser.Add(scene);
+//   visualiser.Render();
+//
+//   return 0;
+//}
 
 //  visualiser.Shaders.emplace_back("libs/Visualiser/resources/shaders/Line.glsl");
 //  visualiser.Shaders[0].Bind();
