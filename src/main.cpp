962e773b76138c5f61ba6b73fd955353dd88837a
Byte-Sized Euclid <8bit.euclid@gmail.com>
2022-09-06 15:53:04 +0200

    Boolean operations and levelset offset working. Yet to implement LS advection.

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,9 @@
 using namespace aprn;
 
 #include <openvdb/openvdb.h>
-#include <openvdb/tools/LevelSetSphere.h>
+#include <openvdb/tools/Composite.h>
 #include <openvdb/tools/LevelSetAdvect.h>
+#include <openvdb/tools/ValueTransformer.h>
 #include <execution>
 
 using namespace openvdb;
@@ -80,24 +81,35 @@ void createAndSaveCylinder()
 
    // Make cylinder 0.
    const float r0 = 2.5f;
-   const Vec3d c0 = {5.0, 0.0, 0.0};
+   const Vec3d c0 = {2.5, 0.0, 0.0};
    makeCylinder(grid0, r0, c0, indexBB, h, background_value);
    grid0->setName("LevelSetCylinder0");
 //   grid0->setGridClass(openvdb::GRID_LEVEL_SET);
 
-   const openvdb::Vec3f s0 = {0.0, 0.0, 0.0};
-   const openvdb::Vec3f s1 = {2.5, 0.0, 0.0};
-   const openvdb::Vec3f s2 = {5.0, 0.0, 0.0};
-   Print("Grid0 Values:", SamplePoint(grid0, s0), SamplePoint(grid0, s1), SamplePoint(grid0, s2));
+   const openvdb::Vec3f s0 = {-5.0, 0.0, 0.0};
+   const openvdb::Vec3f s1 = {-2.5, 0.0, 0.0};
+   const openvdb::Vec3f s2 = { 0.0, 0.0, 0.0};
+   const openvdb::Vec3f s3 = { 2.5, 0.0, 0.0};
+   const openvdb::Vec3f s4 = { 5.0, 0.0, 0.0};
+   Print("Grid0 Values:", SamplePoint(grid0, s0), SamplePoint(grid0, s1), SamplePoint(grid0, s2), SamplePoint(grid0, s3), SamplePoint(grid0, s4));
 
    // Make cylinder 1.
    const float r1 = 2.5f;
-   const Vec3d c1 = {-5.0, 0.0, 0.0};
+   const Vec3d c1 = {-2.5, 0.0, 0.0};
    makeCylinder(grid1, r1, c1, indexBB, h, background_value);
    grid1->setName("LevelSetCylinder1");
 //   grid1->setGridClass(openvdb::GRID_LEVEL_SET);
+   Print("Grid1 Values:", SamplePoint(grid1, s0), SamplePoint(grid1, s1), SamplePoint(grid1, s2), SamplePoint(grid1, s3), SamplePoint(grid1, s4));
 
-   Print("Grid1 Values:", SamplePoint(grid1, s0), SamplePoint(grid1, s1), SamplePoint(grid1, s2));
+   openvdb::tools::csgUnion(*grid0, *grid1);
+//   openvdb::tools::csgIntersection(*grid0, *grid1);
+//   openvdb::tools::csgDifference(*grid0, *grid1);
+   Print("Grid Combination Values:", SamplePoint(grid0, s0), SamplePoint(grid0, s1), SamplePoint(grid0, s2), SamplePoint(grid0, s3), SamplePoint(grid0, s4));
+
+   // Define a functor that offsets the levelset.
+   const double offset = -1.0;
+   auto func = [&offset](const openvdb::FloatGrid::ValueAllIter& iter){ iter.setValue(iter.getValue() - offset); };
+   openvdb::tools::foreach(grid0->beginValueAll(), func);
 
    // Save grid to file
    openvdb::io::File file("mygrids.vdb");
