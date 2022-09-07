9b09b8419de006ed5b385728a094a337476f439b
Byte-Sized Euclid <8bit.euclid@gmail.com>
2022-09-07 16:30:50 +0200

    Implemented a working advection scheme, although super slow.

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,9 @@
 using namespace aprn;
 
 #include <openvdb/openvdb.h>
+#include <openvdb/math/FiniteDifference.h>
 #include <openvdb/tools/Composite.h>
+#include <openvdb/tools/GridOperators.h>
 #include <openvdb/tools/LevelSetAdvect.h>
 #include <openvdb/tools/ValueTransformer.h>
 #include <execution>
@@ -71,20 +73,22 @@ void createAndSaveCylinder()
 {
    openvdb::initialize();
 
-   float background_value = 10.0;
+   float background_value = 0.7;
    openvdb::FloatGrid::Ptr grid0 = openvdb::FloatGrid::create(background_value);
    openvdb::FloatGrid::Ptr grid1 = openvdb::FloatGrid::create(background_value);
 
    // Common attributes.
-   const double         h  = 0.1;
-   CoordBBox indexBB(Coord(-80, -80, -80), Coord(80, 80, 80));
+//   const double h  = 0.2;
+//   CoordBBox indexBB(Coord(-40, -40, -40), Coord(40, 40, 40));
+   const double h  = 0.1;
+   CoordBBox indexBB(Coord(-80, -80, -5), Coord(80, 80, 5));
 
    // Make cylinder 0.
    const float r0 = 2.5f;
-   const Vec3d c0 = {2.5, 0.0, 0.0};
+   const Vec3d c0 = {2.75, 0.0, 0.0};
    makeCylinder(grid0, r0, c0, indexBB, h, background_value);
    grid0->setName("LevelSetCylinder0");
-//   grid0->setGridClass(openvdb::GRID_LEVEL_SET);
+   grid0->setGridClass(openvdb::GRID_LEVEL_SET);
 
    const openvdb::Vec3f s0 = {-5.0, 0.0, 0.0};
    const openvdb::Vec3f s1 = {-2.5, 0.0, 0.0};
@@ -95,10 +99,10 @@ void createAndSaveCylinder()
 
    // Make cylinder 1.
    const float r1 = 2.5f;
-   const Vec3d c1 = {-2.5, 0.0, 0.0};
+   const Vec3d c1 = {-2.75, 0.0, 0.0};
    makeCylinder(grid1, r1, c1, indexBB, h, background_value);
    grid1->setName("LevelSetCylinder1");
-//   grid1->setGridClass(openvdb::GRID_LEVEL_SET);
+   grid1->setGridClass(openvdb::GRID_LEVEL_SET);
    Print("Grid1 Values:", SamplePoint(grid1, s0), SamplePoint(grid1, s1), SamplePoint(grid1, s2), SamplePoint(grid1, s3), SamplePoint(grid1, s4));
 
    openvdb::tools::csgUnion(*grid0, *grid1);
@@ -106,10 +110,26 @@ void createAndSaveCylinder()
 //   openvdb::tools::csgDifference(*grid0, *grid1);
    Print("Grid Combination Values:", SamplePoint(grid0, s0), SamplePoint(grid0, s1), SamplePoint(grid0, s2), SamplePoint(grid0, s3), SamplePoint(grid0, s4));
 
-   // Define a functor that offsets the levelset.
-   const double offset = -1.0;
-   auto func = [&offset](const openvdb::FloatGrid::ValueAllIter& iter){ iter.setValue(iter.getValue() - offset); };
-   openvdb::tools::foreach(grid0->beginValueAll(), func);
+//   // Define a functor that offsets the levelset.
+//   const double offset = -1.0;
+//   auto func = [&offset](const openvdb::FloatGrid::ValueAllIter& iter){ iter.setValue(iter.getValue() - offset); };
+//   openvdb::tools::foreach(grid0->beginValueAll(), func);
+
+   // Compute the gradient grid.
+   const VectorGrid::Ptr gradient_grid = openvdb::tools::gradient(*grid0);
+
+   // Advect the level-set field.
+   openvdb::tools::DiscreteField<VectorGrid, openvdb::tools::QuadraticSampler> velocity_field(*gradient_grid);
+   openvdb::tools::LevelSetAdvection levelset_advector(*grid0, velocity_field);
+   levelset_advector.setSpatialScheme(openvdb::math::HJWENO5_BIAS);
+   levelset_advector.setTemporalScheme(openvdb::math::TVD_RK2);
+   levelset_advector.setTrackerSpatialScheme(openvdb::math::HJWENO5_BIAS);
+   levelset_advector.setTrackerTemporalScheme(openvdb::math::TVD_RK1);
+
+   const float dT = 0.7;
+   const float dt = 0.01;
+   uint iter{};
+   for(float t = 0; t < dT; t += dt) Print("Iter:", iter++, "\tTime:", t, "\tSteps:", levelset_advector.advect(t, t + dt));
 
    // Save grid to file
    openvdb::io::File file("mygrids.vdb");
